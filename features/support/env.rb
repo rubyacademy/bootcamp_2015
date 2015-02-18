f8892f3b516981daaa74821380c36f5999b65545
Mark <mark.wing@us.sogeti.com>
2015-02-18 15:15:55 -0500

    Cleaned up env. rb file

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,5 @@
 require 'watir-webdriver'
 require 'cucumber'
 
-require_relative '../../features/support/helper_methods'
-
+require_relative 'helper_methods.rb'
 BROWSER = HelperMethods.launch_browser
